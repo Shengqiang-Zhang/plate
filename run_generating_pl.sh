ad75e8a4becbc562ea520d76d34ae7705f793324
Shengqiang-Zhang <zhangsq5829@gmail.com>
2021-06-12 13:42:13 +0800

    fix pred.py bug

--- run_generating_pl.sh
+++ run_generating_pl.sh
@@ -2,20 +2,23 @@
 split -l 10000 /path/to/x_dataset/train.source -d -a 3 /path/to/x_dataset/train_split/train.source
 
 DATASET=cnndm_fullstops
-CKPT_PATH=/path/to/ckpt_dir/
-OUTPUT_DIR=/path/to/x_dataset/train_split_output/
-RETAIN_DROPOUT=False
+CKPT_DIR=/path/to/ckpt_dir/
+CKPT_FILE=model.pt
+TEST_FILE=/path/to/x_dataset/raw/test.source
+OUTPUT_DIR=/path/to/x_dataset/train_split_output/test.source.pred
+INF_RETAIN_DROPOUT=False
 INFERENCE_PATAMS=4,2.0,140,55,3
+BATCH_SIZE=16
 LOAD_CKPT_DATA=/path/to/cnndm_dataset/bin/
 ATTN_TEMP=128
 
 for file in /path/to/x_dataset/train_split/*; do
 	TEST_FILE=${file##*/}
     OUTPUT_FILE=${OUTPUT_DIR}/${TEST_FILE}.pred
-	python3 pred.py --dataset ${DATASET} --ckpt_path ${CKPT_PATH} \
+	python3 pred.py --ckpt_dir ${CKPT_DIR} --ckpt_file ${CKPT_FILE} \
             --test_file ${TEST_FILE} --output_file ${OUTPUT_FILE} \
             --retain_dropout ${INF_RETAIN_DROPOUT} --inference_params ${INFERENCE_PARAMS} \
-            --load_ckpt_data ${LOAD_CKPT_DATA} \
+            --load_ckpt_data ${LOAD_CKPT_DATA} --batch_size ${BATCH_SIZE} \
             --encoder_attn_temp ${ATTN_TEMP} --decoder_attn_temp ${ATTN_TEMP} \
             --cross_attn_temp ${ATTN_TEMP}
 done
